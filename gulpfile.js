72f7a926cbe21a661d19e8e9e8c729789d4fe40b
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2015-01-09 00:01:04 +0300

    Add Bower deps: preboot2 and cookies-js
    Change Gulp file accordingly

--- gulpfile.js
+++ gulpfile.js
@@ -16,15 +16,25 @@ gulp.task('bower-js', function () {
 
     return gulp.src([
         'vendor/jquery/dist/jquery.min.js',
-        'vendor/lodash/dist/lodash.compat.min.js'
+        'vendor/lodash/dist/lodash.compat.min.js',
+        'vendor/cookies-js/src/cookies.js'
     ])
         .pipe(plugins.concat('vendor.min.js'))
         //.pipe(plugins.uglify()).on('error', onError)
         .pipe(gulp.dest('assets/js'))
 });
 
+gulp.task('bower-less', function () {
+
+    return gulp.src([
+        'vendor/preboot/less/preboot.less'
+    ])
+        .pipe(plugins.flatten())
+        .pipe(gulp.dest('src/less/vendor'))
+});
+
 gulp.task('less', function () {
-    return gulp.src('src/less/*.less')
+    return gulp.src('src/less/app.less')
         .pipe(plugins.less()).on('error', onError)
         .pipe(plugins.csso())
         .pipe(plugins.rename({
@@ -44,7 +54,7 @@ gulp.task('jsmin', function () {
         'src/js/ball.js',
         'src/js/score.js',
         'src/js/animation.js',
-        'src/js/init.js',
+        'src/js/init.js'
     ])
         .pipe(plugins.concat('app.min.js'))
         //.pipe(plugins.uglify()).on('error', onError)
@@ -54,7 +64,7 @@ gulp.task('jsmin', function () {
 gulp.task('watch', function () {
     gulp.watch('src/less/*.less', ['less']);
     gulp.watch('src/js/*.js', ['jsmin']);
-    gulp.watch('bower/**/*', ['bower-js']);
+    gulp.watch('bower/**/*', ['bower-js', 'bower-less']);
 });
 
 gulp.task('serve', function () {
@@ -83,4 +93,4 @@ gulp.task('deploy', function () {
         .pipe(plugins.cloudfiles(config, {'read': false}));
 });
 
-gulp.task('default', ['bower-js', 'less', 'jsmin']);
+gulp.task('default', ['bower-less', 'bower-js', 'less', 'jsmin']);
